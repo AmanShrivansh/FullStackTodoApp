9792262cd8ed4313b1ac60899ce3fbc85b47965d
AmanShrivansh <amanshrivansh04@gmail.com>
2025-01-17 22:59:58 +0530

    Removed hardcoding

--- src/Components/todo/TodoApp.jsx
+++ src/Components/todo/TodoApp.jsx
@@ -1,6 +1,6 @@
 import './TodoApp.css'
 import { useState } from 'react';
-import { BrowserRouter, Routes , Route, useNavigate} from 'react-router-dom';
+import { BrowserRouter, Routes , Route, useNavigate, useParams} from 'react-router-dom';
 
 export default function TodoApp(){
     return (
@@ -9,7 +9,7 @@ export default function TodoApp(){
                 <Routes>
                     <Route path='/' element={<LoginComponent/>}></Route>
                     <Route path='/login' element={<LoginComponent/>}></Route>
-                    <Route path='/welcome' element={<WelcomeComponent/>}></Route>
+                    <Route path='/welcome/:username' element={<WelcomeComponent/>}></Route>
                     <Route path='*' element={<ErrorComponent/>}></Route>
                 </Routes>
             </BrowserRouter>
@@ -37,7 +37,7 @@ function LoginComponent(){
             console.log("success");
             setShowSuccessMessage(true)
             setShowErrorMessage(false)
-            navigate('/welcome')
+            navigate(`/welcome/${username}`)
         }else{
             setShowSuccessMessage(false)
             setShowErrorMessage(true)
@@ -70,9 +70,10 @@ function LoginComponent(){
 }
 
 function WelcomeComponent(){
+    const {username} = useParams()
     return (
         <div className="WelcomeComponent">
-            <h1>Welcome to in28minutes</h1>
+            <h1>Welcome to {username}</h1>
             <div >
                 Welcome Component
             </div>
