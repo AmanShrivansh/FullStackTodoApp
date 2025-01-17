a6b3e14162bb91654d4737808b1c4d66e61ef481
AmanShrivansh <amanshrivansh04@gmail.com>
2025-01-17 21:45:00 +0530

    Enhanced login comp

--- src/Components/todo/TodoApp.jsx
+++ src/Components/todo/TodoApp.jsx
@@ -1,3 +1,5 @@
+import './TodoApp.css'
+import { useState } from 'react';
 export default function TodoApp(){
     return (
         <div clssName="TodoApp">
@@ -10,16 +12,28 @@ export default function TodoApp(){
 }
 
 function LoginComponent(){
+
+    const [username, setUsername] = useState('in28minutes')
+    const [password, setPassword] = useState('')
+
+    function handleUsernameChange(event){
+        setUsername(event.target.value);
+    }
+
+    function handlePasswordChange(event){
+        setUsername(event.target.value);
+    }
+
     return (
         <div className="Login">
             <div className="LoginForm">
                 <div>
                     <label>User Name</label>
-                    <input type="text" name="username" />
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" name="password" />
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
                 </div>
                 <div>
                     <button type="button" name="login">Login</button>
