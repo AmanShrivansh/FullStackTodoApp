8e43b96b45df1aa3538a84d250c209882a9ad2bf
AmanShrivansh <amanshrivansh04@gmail.com>
2025-01-18 00:10:34 +0530

    Added header,footer & logout cmp

--- src/Components/todo/TodoApp.jsx
+++ src/Components/todo/TodoApp.jsx
@@ -1,21 +1,26 @@
 import './TodoApp.css'
 import { useState } from 'react';
-import { BrowserRouter, Routes , Route, useNavigate, useParams} from 'react-router-dom';
+import { BrowserRouter, Routes , Route, useNavigate, useParams, Link} from 'react-router-dom';
 
 export default function TodoApp(){
     return (
         <div clssName="TodoApp">
+            <HeaderComponent/>
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<LoginComponent/>}></Route>
-                    <Route path='/login' element={<LoginComponent/>}></Route>
-                    <Route path='/welcome/:username' element={<WelcomeComponent/>}></Route>
-                    <Route path='*' element={<ErrorComponent/>}></Route>
+                    <Route path='/' element={<LoginComponent/>}/>
+                    <Route path='/login' element={<LoginComponent/>}/>
+                    <Route path='/todos' element={<ListTodosComponent/>}/>
+                    <Route path='/welcome/:username' element={<WelcomeComponent/>}/>
+                    <Route path='/logout' element={<LogoutComponent/>}/>
+                    <Route path='*' element={<ErrorComponent/>}/>
                 </Routes>
             </BrowserRouter>
+            <FooterComponent/>
         </div>
     )
 }
+
 
 function LoginComponent(){
 
@@ -73,9 +78,9 @@ function WelcomeComponent(){
     const {username} = useParams()
     return (
         <div className="WelcomeComponent">
-            <h1>Welcome to {username}</h1>
+            <h1>Welcome {username}</h1>
             <div >
-                Welcome Component
+                Manage your todos - <Link to="/todos">Go here</Link>
             </div>
         </div>
     )
@@ -87,6 +92,76 @@ function ErrorComponent(){
             <h1>We are working hard on the issue.</h1>
             <div>
                 Apologies for the 404. Reach out to team @ amanshrivansh@gmail.com.
+            </div>
+        </div>
+    )
+}
+
+function ListTodosComponent(){
+    const today = new Date();
+    const targetDate = new Date(today.getFullYear()+1 , today.getMonth(), today.getDay())
+
+    const todos = [
+                    {id:1,description:'Learn AWS', done: false, targetDate:targetDate},
+                    {id:2,description:'Learn Full stack dev', done: false, targetDate:targetDate},
+                    {id:3,description:'Learn AWS', done: false, targetDate:targetDate}
+                ]
+    return (
+        <div className="ListTodosComponent">
+            <h1>Things you want to do</h1>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>ID</td>
+                            <td>Description</td>
+                            <td>Is Done?</td>
+                            <td>Target Date</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            todos.map(
+                                todos => (
+                                    <tr key={todos.id}>
+                                        <td>{todos.id}</td>
+                                        <td>{todos.description}</td>
+                                        <td>{todos.done.toString()}</td>
+                                        <td>{todos.targetDate.toDateString()}</td>
+                                    </tr>
+                                )
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+
+function HeaderComponent(){
+    return (
+        <div className="header">
+               Header <hr/>
+        </div>
+    )
+}
+
+function FooterComponent(){
+    return (
+        <div className="footer">
+            <hr/>Footer
+        </div>
+    )
+}
+
+function LogoutComponent(){
+    return (
+        <div className="LogoutComponent">
+            <h1>You are logged out!</h1>
+            <div>
+                Thank you for using our app. Come back soon!
             </div>
         </div>
     )
