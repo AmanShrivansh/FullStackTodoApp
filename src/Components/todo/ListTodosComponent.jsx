234a28e97817922abdef114170dd1cb55223bc3b
AmanShrivansh <amanshrivansh04@gmail.com>
2025-01-18 23:39:08 +0530

    Displaying todos from rest api

--- src/Components/todo/ListTodosComponent.jsx
+++ src/Components/todo/ListTodosComponent.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
+import { retrieveAllTodosForUsername } from "./api/TodoApiService";
+
 export default function ListTodosComponent(){
     const today = new Date();
     const targetDate = new Date(today.getFullYear()+1 , today.getMonth(), today.getDay())
 
-    const todos = [
-                    {id:1,description:'Learn AWS', done: false, targetDate:targetDate},
-                    {id:2,description:'Learn Full stack dev', done: false, targetDate:targetDate},
-                    {id:3,description:'Learn AWS', done: false, targetDate:targetDate}
-                ]
+    const [todos,setTodos] = useState([])
+
+    // const todos = [
+    //                 // {id:1,description:'Learn AWS', done: false, targetDate:targetDate},
+    //                 // {id:2,description:'Learn Full stack dev', done: false, targetDate:targetDate},
+    //                 // {id:3,description:'Learn AWS', done: false, targetDate:targetDate}
+    //             ]
+
+    useEffect ( () => refreshTodos(), [] )
+
+    function refreshTodos(){
+        retrieveAllTodosForUsername('in28minutes')
+        .then(response => {
+            setTodos(response.data)
+        })
+        .catch(error => console.log(error))
+    }
+
     return (
         <div className="container">
             <h1>Things you want to do</h1>
@@ -28,7 +44,7 @@ export default function ListTodosComponent(){
                                         <td>{todos.id}</td>
                                         <td>{todos.description}</td>
                                         <td>{todos.done.toString()}</td>
-                                        <td>{todos.targetDate.toDateString()}</td>
+                                        <td>{todos.targetDate.toString()}</td>
                                     </tr>
                                 )
                             )
--- src/Components/todo/WelcomeComponent.jsx
+++ src/Components/todo/WelcomeComponent.jsx
@@ -1,23 +1,28 @@
 import {useParams, Link} from 'react-router-dom';
-import axios from 'axios';
+import { useState } from 'react';
+import { retrieveHelloPathvariable } from './api/HelloWorldApiService';
 
-function callHelloWorldRestApi(){
-    axios.get("http://localhost:8080/hello-world")
-        .then((response)=>successfullResponse(response))
-        .catch((error)=>errorResponse(error))
-        .finally(()=>console.log("cleanup"))
-}
-
-function successfullResponse(response){
-    console.log(response)
-}
-
-function errorResponse(error){
-    console.log(error)
-}
 
 function WelcomeComponent(){
     const {username} = useParams()
+    const [message,setMesage] = useState(null)
+
+    function callHelloWorldRestApi(){
+        retrieveHelloPathvariable('Aman')
+       .then((response)=>successfullResponse(response))
+       .catch((error)=>errorResponse(error))
+       .finally(()=>console.log("cleanup"))
+   }
+
+    function successfullResponse(response){
+        console.log(response)
+        setMesage(response.data.message)
+    }
+
+    function errorResponse(error){
+        console.log(error)
+    }
+
     return (
         <div className="WelcomeComponent">
             <h1>Welcome {username}</h1>
@@ -25,7 +30,7 @@ function WelcomeComponent(){
                 Manage your todos - <Link to="/todos">Go here</Link>
             </div>
             <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>Call hello world </button>
-            
+            <div className="text-info">{message}</div>
         </div>
     )
 }
